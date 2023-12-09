21168e10f937d5cb746398a5b98df2352ebfb1a4
Macbook Beks <babdikaarov@gmail.com>
2023-12-09 08:23:24 +0300

    deploy issue 404

--- src/App.tsx
+++ src/App.tsx
@@ -11,14 +11,11 @@ import Main from "./pages/main";
 
 const appRouter = createBrowserRouter(
   createRoutesFromElements(
-    <>
-      <Route path="/" element={<CoolBand />}>
-        <Route index={true} element={<Main />}></Route>
-        <Route path="/event" element={<NotFoundPage />}></Route>
-        <Route path="/events" element={<NotFoundPage />}></Route>
-      </Route>
-      <Route path="*" element={<NotFoundPage />}></Route>
-    </>
+    <Route path="/" element={<CoolBand />}>
+      <Route index={true} element={<Main />}></Route>
+      <Route path="/event" element={<NotFoundPage />}></Route>
+      <Route path="/events" element={<NotFoundPage />}></Route>
+    </Route>
   )
 );
 
